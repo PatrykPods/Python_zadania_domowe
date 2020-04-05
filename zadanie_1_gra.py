81f5d97d3d7a45ca9c37d2c4ed35defe31fddbde
patrykpods <patryk.podsiadlo@gmail.com>
2020-04-05 17:59:39 +0200

    poprawka nr2

--- zadanie_1_gra.py
+++ zadanie_1_gra.py
@@ -1,30 +1,33 @@
 while True:
     x = int(input("podaj pozycje gracza X:"))
     y = int(input("podaj pozycje gracza Y:"))
-    if x >= 1 and x <= 100 and y >= 1 and y <= 100 :
 
-        if x <= 10 and y <= 10 :
-             print("gracz znajduje sie w lewym dolnym rogu")
-        elif x >= 90 and y <= 10 :
-            print("gracz znajduje sie w prawym dolnym rogu")
-        elif x <= 10 and y >= 90 :
-            print("gracz znajduje sie w lewym gornym rogu")
-        elif x >= 90 and y >= 90 :
-            print("gracz znajduje sie w prawym gornym rogu")
-        elif x > 10 and y <= 10 :
-            print("gracz znajduje sie na dolnej krawedzi")
-        elif x > 10 and y >= 90 :
-            print("gracz znajduje sie na gornej krawedzi")
-        elif x <= 10 and y > 10 :
-            print("gracz znajduje sie na lewej krawedzi")
-        elif x >= 90 and y > 10 :
-            print("gracz znajduje sie na prawej krawedzi")
-        else :
-            print("gracz znajduje sie w centrum")
+    if x > 100 or y > 100 :
+        print("gracz jest poza plansza")
+
+    elif x <= 10 and y <= 10 :
+         print("gracz znajduje sie w lewym dolnym rogu")
+    elif x >= 90 and y <= 10 :
+        print("gracz znajduje sie w prawym dolnym rogu")
+    elif x <= 10 and y >= 90 :
+        print("gracz znajduje sie w lewym gornym rogu")
+    elif x >= 90 and y >= 90 :
+        print("gracz znajduje sie w prawym gornym rogu")
+    elif x > 10 and y <= 10 :
+        print("gracz znajduje sie na dolnej krawedzi")
+    elif x > 10 and y >= 90 :
+        print("gracz znajduje sie na gornej krawedzi")
+    elif x <= 10 and y > 10 :
+        print("gracz znajduje sie na lewej krawedzi")
+    elif x >= 90 and y > 10 :
+        print("gracz znajduje sie na prawej krawedzi")
     else :
-        print("gracz znajduje sie poza plansza")
+        print("gracz znajduje sie w centrum")
+
 
     q = input("czy chcesz kontynowac? tak/nie")
 
     if q == "nie"  :
         break
+
+
