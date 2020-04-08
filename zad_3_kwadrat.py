c8e9b5fad053ad86a242ecc5182cd3708e182f55
patrykpods <patryk.podsiadlo@gmail.com>
2020-04-08 20:06:46 +0200

    poprawka

---
+++ zad_3_kwadrat.py
@@ -0,0 +1,10 @@
+counter = int(input("podaj liczbe:"))
+
+x = 0
+
+while x < counter :
+    if x == 0 or x == counter - 1 :
+        print("*" * counter)
+    elif x > 0 and x < counter :
+        print("*" + " " * (counter - 2) + "*")
+    x += 1
